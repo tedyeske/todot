5830f28a7fba6972682cf45dfad32855013b975e
tewodros <teddyhailu5437@gmail.com>
2020-05-16 22:42:19 +0000

    set-up

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'rspec-rails', '~> 3.5'
+  gem "factory_bot_rails"
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
@@ -55,6 +57,6 @@ group :test do
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
 end
-
+gem 'jquery-rails'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+RSpec.describe TasksController, type: :controller do
+  describe "tasks#index" do
+    it "should list the tasks in the database" do
+      task1 = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task)
+      get :index
+      assert_response :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2)
+    end
+  end
+
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryBot.create(:task, done: false)
+      put :update, params: {id: task.id, task: { done: true }}
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
+  end
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
+end
